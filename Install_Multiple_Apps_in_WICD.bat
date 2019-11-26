3b53a49099714b549eb417992838d9b9d6b094f7
Pierre Emmanuel JOUBERT <pierre@redkaffe.com>
2019-11-26 16:23:45 +0100

    Update Install_Multiple_Apps_in_WICD.bat
    hfd

--- Install_Multiple_Apps_in_WICD.bat
+++ Install_Multiple_Apps_in_WICD.bat
@@ -3,11 +3,10 @@ IF NOT EXIST "C:\Temp\ApplicationLogs" MD "C:\Temp\ApplicationLogs"
 REM Define Logfiles
 set LOGFILE1=%SystemDrive%\Temp\ApplicationLogs\WSO_Client.log
 set LOGFILE2=%SystemDrive%\Temp\ApplicationLogs\AV_Client.log
-set LOGFILE3=%SystemDrive%\Temp\ApplicationLogs\AdobeReaderDC.log
 
 REM Install MSI
 echo Install VM Ware Workspace One Client >> %LOGFILE1%
-msiexec /i "AirWatchAgent.msi" /q ENROLL=Y SERVER=ds112.airwatchportals.com LGName=WKSWSO USERNAME=staging@wkswso.com PASSWORD=E+9kxu DOWNLOADWSBUNDLE=TRUE /LOG %LOGFILE1%
+msiexec /i "AirWatchAgent.msi" /q ENROLL=Y SERVER=users.somobile.sodexonet.com LGName=WKSWSO USERNAME=staging@wkswso.com PASSWORD=E+9kxu DOWNLOADWSBUNDLE=TRUE /LOG %LOGFILE1%
 echo result: %ERRORLEVEL% >> %LOGFILE1%
 
 REM Install MSI
@@ -15,7 +14,3 @@ echo Install AV Client >> %LOGFILE2%
 msiexec /i "GRP.HQ_SmartScan.msi" /qn /norestart /l
 echo result: %ERRORLEVEL% >> %LOGFILE2%
 
-REM Install EXE
-echo Install AdobeReaderDC >> %LOGFILE3%
-SetupAdobeAcrobatDC.exe /sAll /qf /quiet %LOGFILE3%
-echo result: %ERRORLEVEL% >> %LOGFILE3%
